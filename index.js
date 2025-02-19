aa6e87ff4a77368c89e9a7edda405253a6966d7f
Giorgiana Bîrsan <64731577+GiorgianaBirsan@users.noreply.github.com>
2025-02-19 15:14:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
 
 var app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),function(req,res){
+     
+ //    console.log("file",req.file)
 
+     res.send({"name":req.file.originalname,"type":req.file.mimetype,"size":req.file.size})
+})
 
 
 const port = process.env.PORT || 3000;
